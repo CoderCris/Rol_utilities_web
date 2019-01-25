386f0b558c293e70d5eb2b0e136c198d466acfd7
Crisamer Ortega González <crisamer.ortega@gmail.com>
2019-01-25 15:23:50 +0000

    some tries

--- functions.js
+++ functions.js
@@ -1,3 +1,12 @@
 function roll(){
-	return Math.random() * (document.getElementById("faces").innerHTML);
+	
+
+	var para = document.createElement("p");
+	var value = Math.floor(Math.random() * (document.getElementById("faces").innerHTML));
+	para.appendChild(value);
+	document.getElementById("rollResult").appendChild(para);
+}
+
+function dummie(){
+	document.getElementById("rollResult").innerHTML = 'It works!!'
 }
